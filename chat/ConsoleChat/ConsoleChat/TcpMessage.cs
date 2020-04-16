c6dbb30bc70f19c0874de4e088b35ed5c9b7df0b
haid_roman <haidroman@gmail.com>
2020-04-17 00:00:00 +0300

    without history

---
+++ chat/ConsoleChat/ConsoleChat/TcpMessage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChat
+{
+    public enum TcpTypes : byte
+    {
+        Name = 0,
+        Message = 1,
+        Close = 2,
+        History = 3,
+        HistoryTask = 4
+    };
+
+    class TcpMessage
+    {
+        private byte Type = 5;
+        private byte DataLength = 0;
+        private byte[] Data = { };
+
+
+        public TcpMessage(byte type, string data)
+        {
+            if (type == 0 || type == 1 || type == 2 || type == 3 || type == 4)
+            {
+                Data = Encoding.Unicode.GetBytes(data);
+                if (Data.Length < 256)
+                {
+                    DataLength = (byte)Data.Length;
+                    Type = type;
+                }
+            }
+        }
+
+        public TcpMessage(byte[] bytes)
+        {
+            if ((bytes[0] == 0 || bytes[0] == 1 || bytes[0] == 2 || bytes[0] == 3 || bytes[0] == 4) && bytes[1] == bytes.Length - 2)
+            {
+                Array.Resize(ref Data, bytes.Length - 2);
+                Array.Copy(bytes, 2, Data, 0, bytes.Length - 2);
+                if (Data.Length < 256)
+                {
+                    DataLength = (byte)Data.Length;
+                    Type = bytes[0];
+                }
+            }
+        }
+
+        public bool CheckMessage(ref byte type)
+        {
+            type = Type;
+            if (Enum.IsDefined(typeof(UdpTypes), Type) && Data.Length == DataLength)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public string GetData()
+        {
+            return Encoding.Unicode.GetString(Data);
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] bytes = new byte[DataLength + 2];
+            bytes[0] = Type; bytes[1] = DataLength;
+            Array.Copy(Data, 0, bytes, 2, DataLength);
+            return bytes;
+        }
+    }
+}
---
+++ chat/ConsoleChat/ConsoleChat/UdpMessage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChat
+{
+    public enum UdpTypes : byte
+    {   
+        Disconnected = 0,
+        Connected = 1
+    };
+    public class UdpMessage
+    {
+        private byte Type = 2;
+        private byte NameLength = 0;
+        private byte[] Name = { };
+
+ 
+        public UdpMessage(byte type, string name)
+        {
+            if ((type == 0 || type == 1))
+            {
+                Name = Encoding.Unicode.GetBytes(name);
+                if (Name.Length < 256)
+                {
+                    NameLength = (byte)Name.Length;
+                    Type = type;
+                }
+            }
+        }
+
+        public UdpMessage(byte[] bytes)
+        {
+            if ((bytes[0] == 0 || bytes[0] == 1) && bytes[1] == bytes.Length - 2)
+            {
+                Array.Resize(ref Name, bytes.Length - 2);
+                Array.Copy(bytes, 2, Name, 0, bytes.Length - 2);
+                if (Name.Length < 256)
+                {
+                    NameLength = (byte)Name.Length;
+                    Type = bytes[0];
+                }
+            }
+        }
+
+        public bool CheckMessage(ref byte type)
+        {
+            type = Type;
+            if (Enum.IsDefined(typeof(UdpTypes), Type) && Name.Length == NameLength)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string GetName()
+        {
+            return Encoding.Unicode.GetString(Name);
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] bytes = new byte[NameLength + 2];
+            bytes[0] = Type; bytes[1] = NameLength;
+            Array.Copy(Name, 0, bytes, 2, NameLength);
+            return bytes;
+        }
+    }
+}
